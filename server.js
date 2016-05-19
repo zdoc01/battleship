5e6567e24c679d7eec872783f389957d7e9aa0c5
Zachary Lumpkins <zdoc01@gmail.com>
2016-05-18 23:42:03 -0700

    adding twitter client with initial twitter integration

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var TwitterClient = require('./twitter-client');
 
 var PORT = parseInt(process.argv[2], 10) || 9000;
+var screenNameRegex = /^@battleshipbot/;
+var TC = new TwitterClient();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,10 +20,20 @@ io.on('connection', function(socket) {
 
 	socket.on('newMessage', function(message) {
 		console.log('new message received!', message);
+
+		// Allow a user to guess a location in the chat
+		// window (e.g. '@battleshipbot D15')
+		if (screenNameRegex.test(message) && TC.started) {
+			TC.postTweet(message);
+		}
+
 		// echo globally (all clients)
 		socket.broadcast.emit('newMessage', message);
 	});
 });
+
+// start listening for tweets
+TC.start();
 
 http.listen(PORT, function() {
 	console.log('Server listening on port %s', PORT);
---
+++ twitter-client.js
@@ -0,0 +1,75 @@
+var Twitter = require('twitter');
+var ENV = process.env;
+
+function handleError(error) {
+	if (error) {
+		this.started = false;
+		throw error;
+	}
+}
+
+function handleStreamEnd(resp) {
+	console.log('The stream has ended...\n');
+	console.log(resp);
+	this.started = false;
+}
+
+function handleTweet(tweet) {
+	var user, message;
+
+	if (tweet) {
+		message = tweet.text;
+		user = tweet.user && tweet.user.screen_name;
+		this.following = (tweet.friends) ? tweet.friends : this.following;
+	}
+
+	if (user && message) {
+		// TODO - update game state
+	}
+}
+
+function TwitterClient(opts) {
+	opts = opts || {};
+	var tweetHandler = (typeof opts.handleTweet === 'function') ? opts.handleTweet : handleTweet;
+
+	/**
+	 * Cache the accounts we're following since
+	 * they are only provided when the stream is
+	 * initialized.
+	 * 
+	 * @type {Array}
+ 	 */
+	this.following = [];
+
+	this.client = new Twitter({
+		consumer_key: ENV.TWITTER_CONSUMER_KEY,
+		consumer_secret: ENV.TWITTER_CONSUMER_SECRET,
+		access_token_key: ENV.TWITTER_ACCESS_TOKEN,
+		access_token_secret: ENV.TWITTER_ACCESS_TOKEN_SECRET
+	});
+
+	this.started = false;
+	this.handleTweet = tweetHandler.bind(this);
+	this.handleError = handleError.bind(this);
+	this.handleStreamEnd = handleStreamEnd.bind(this);
+};
+
+TwitterClient.prototype.start = function() {
+	console.log('starting the client...');
+	var self = this;
+	this.started = true;
+	this.client.stream('user', function(stream) {
+		stream.on('data', self.handleTweet);
+		stream.on('error', self.handleError);
+		stream.on('end', self.handleStreamEnd);
+	});
+};
+
+TwitterClient.prototype.postTweet = function(tweet) {
+	if (tweet) {
+		console.log('posting a tweet... - [ %s ]', tweet);
+		this.client.post('statuses/update', {status: tweet}, handleError);
+	}
+};
+
+module.exports = TwitterClient;
